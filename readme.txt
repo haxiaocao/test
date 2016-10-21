16fba029f7886ccb6832c9d6aee405cbe379246f
ixaocao <guokai2007@163.com>
2016-10-21 10:05:09 +0800

    add test.py file

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 this is the first test file for github repository. 
 Hello world!
+
+just for modify
---
+++ test.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+a=np.arange(30).reshape(5,6)
+print a
+print a.shape
+print a.dtype
